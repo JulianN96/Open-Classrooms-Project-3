ef40a7ff7a6127183dbf91030c6c0f338d1b93be
Julian Norris <julian.norris96@gmail.com>
2023-07-04 16:04:24 +0200

    -Completed filter logic to filter works on the page

--- FrontEnd/scripts.js
+++ FrontEnd/scripts.js
@@ -1,8 +1,12 @@
 const api = "http://localhost:5678/api"
 let activeFilters = []
 
+const gallery = document.getElementsByClassName("gallery")
 const filterButtons = document.getElementsByClassName("filters__button")
+const works = document.getElementsByClassName("work")
 
+
+//Filter Logic
 for (i=0; i < filterButtons.length; i++){
     filterButtons[i].addEventListener("click", (e) =>{
 
@@ -15,34 +19,56 @@ for (i=0; i < filterButtons.length; i++){
                 }
             }
             activeFilters = newActiveFilters
-            console.log(activeFilters)
+
         } else{
             e.target.classList.add("filters__button--active")
             if (activeFilters.length > 0){
                 let newActiveFilters = []
+                console.log("activefilters" + activeFilters)
+                newActiveFilters.push(+e.target.id)
                 for (filter of activeFilters){
-                    if(filter != e.target.id){
-                        newActiveFilters.push(+e.target.id)
+                    console.log("targetID " + e.target.id)
+                    if(filter != +e.target.id){
+                        newActiveFilters.push(filter)
                     }
                 }
                 activeFilters = newActiveFilters
-                console.log(activeFilters)  
             } else {
-                activeFilters.push(+e.target.id)
-                console.log(activeFilters)  
+                activeFilters.push(+e.target.id)  
             }
-            if(e.target.id === "0"){  
+            if(e.target.id == 0){  
                 for (button of filterButtons){
                     button.classList.remove("filters__button--active")
                 }
-                activeFilters.push(5)
+                activeFilters = []
             }
         } 
 
+        //disappear logic goes here
+        
+        if (activeFilters.length === 0){
+            for(work of works){
+                work.style.display="block"
+            }
+        }else{
+            let showWorksArray = []
+            for(work of works){
+                for(filter of activeFilters){
+                    if(work.classList.contains(`category${filter}`)){
+                        showWorksArray.push(work)
+                    }else{
+                        work.style.display="none"
+                    }
+                }
+            }
+            console.log(showWorksArray)
+            for (showWork of showWorksArray){
+                showWork.style.display="block"
+            }
+        }
     })
 }
 
-const gallery = document.getElementsByClassName("gallery")
 //Global function to interact with API
 
 function makeRequest (verb, url, data){
@@ -87,7 +113,7 @@ async function getWorks(){
         const worksData = await makeRequest("GET", api + "/works")
         for (work of worksData){
             gallery[0].innerHTML += `
-            <figure>
+            <figure class="work category${work.categoryId}">
                 <img src="${work.imageUrl}" alt="${work.title}">
                 <figcaption>${work.title}</figcaption>
             </figure>
